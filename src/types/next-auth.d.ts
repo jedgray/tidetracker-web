7302a49201fee3fd839ac01491a77de1d1061db1
jedgray <jedgray@gmail.com>
2026-03-20 00:53:59 -0700

    Fix next-auth type declaration

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,14 +1,18 @@
 import 'next-auth'
 import 'next-auth/jwt'
 
-declare module 'next-auth/jwt' {
-  interface JWT {
-    id:                 string
-    disclaimerAccepted: boolean
-    unitHeight:         string
-    unitVelocity:       string
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id:                 string
+      name?:              string | null
+      email?:             string | null
+      image?:             string | null
+      disclaimerAccepted: boolean
+      unitHeight:         string
+      unitVelocity:       string
+    }
   }
-}
   interface User {
     id: string
   }
@@ -16,6 +20,9 @@ declare module 'next-auth/jwt' {
 
 declare module 'next-auth/jwt' {
   interface JWT {
-    id: string
+    id:                 string
+    disclaimerAccepted: boolean
+    unitHeight:         string
+    unitVelocity:       string
   }
 }
